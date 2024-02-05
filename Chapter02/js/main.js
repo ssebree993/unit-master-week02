e53264a81df273846c5a0ecc32d9b93c479d65d5
ssebree993 <157660275+ssebree993@users.noreply.github.com>
2024-02-04 21:45:33 -0600

    upload all changes. AJAX_Leaflet_Get.html is working.

--- Chapter02/js/main.js
+++ Chapter02/js/main.js
@@ -1,5 +1,3 @@
-// Add all scripts to the JS folder
-
 // Replace getElementByID with querySelector()
 // EG: Line 01 var myDiv =  document.querySelector("#mydiv");
 // EG Line 02  myDiv.appendChild(table);
@@ -18,11 +16,31 @@ myDiv.innerHTML = "Welcome to my web page getElementById method";
 window.onload = myFunc(); 
 */
 
-//AJA request for data test
 
 function jsAjax(){
-    // Step 1: Define the data request
-    var request = new Request('data/MegaCities.geojson');
+    // Step 1: Create the data request 
+    var request = new Request('http://127.0.0.1:5500/data/MegaCities.geojson');
+    //Step 2: define Fetch parameters 
+    var init = {
+        method: 'GET'
+    }
+    //Step 3: use Fetch to retrieve data
+    fetch(request, init)
+        .then(conversion) //Step 4 convert data to usable form
+        .then(callback) //Step 5 Send retrieved data to a callback function
 };
+
+//define conversion callback function
+function conversion(response){
+  //convert data to usable form
+  return response.json();
+}
+
+//define callback function
+function callback(response){
+    //tasks using the data go here
+    console.log(response) //print data to the console - don't see it in the browser
+    //http://127.0.0.1:5500/index.html is "Live Server" in VS Code.???????
+}
 
 window.onload = jsAjax();
